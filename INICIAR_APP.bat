e5275607c87ae422ff810d00de1916c6b9cfc71e
JHubilla <sanderacalmon@gmail.com>
2026-09-08 21:45:12 -0300

    Initial commit: FitCheck Provador Virtual

---
+++ INICIAR_APP.bat
@@ -0,0 +1,37 @@
+﻿@echo off
+title FitCheck - Provador Virtual com IA
+chcp 65001 >nul
+cd /d "%~dp0"
+
+echo =======================================================
+echo          FITCHECK - PROVADOR VIRTUAL IE
+echo ======================================================
+echo.
+
+where node >nul 2>nul
+if %ERRORLEVEL% neq 0 (
+    echo [ERRO] Node.js nao foi encontrado no sistema!
+    echo Por favor, instale o Node.js em https://nodejs.org/
+    echo.
+    pause
+    exit /b 1
+)
+
+if not exist "node_modules\" (
+    echo [INFO] Primeira execucao detectada. Instalando dependencias...
+    call npm install
+    if %ERRORLEVEL% neq 0 (
+        echo [ERRO] Falha ao instalar dependencias do projeto.
+        pause
+        exit /b 1
+    )
+)
+
+echo [INFO] Iniciando o FitCheck na porta 3000...
+echo [INFO] Abrindo o navegador em http://localhost:3000
+echo.
+
+start "" "http://localhost:3000"
+call npm run dev -- --port 3000 --host
+
+pause
